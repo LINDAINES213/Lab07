27bea2087b84f0e88acc6f076c54b4a1cc855a60
Mario Guerra <gue21008@uvg.edu.gt>
2022-04-27 08:50:59 -0600

    Creación inicial de archivos

---
+++ Cadenas.s
@@ -0,0 +1,17 @@
+/* -----------------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA 
+* Organización de computadoras y Assembler
+* Ciclo 1 - 2022
+* Mario Antonio Guerra Morales - Carné: 21008
+* Linda Inés Jiménez Vides - Carné: 21169
+* Laboratorio06.s
+* Programa que calcula el perímetro de 3 cuadrados cuyos valores están
+* almacenados en memoria.
+---------------------------------------------- */
+
+.text
+.align 2
+.global main
+.type main,%function
+
+main:
